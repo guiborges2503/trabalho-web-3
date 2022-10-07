0aa0632cb01d5e58c7951c8acedc0a6ca6bcca9f
Guilherme <guilhermetravain1@gmail.com>
2022-10-07 12:20:53 -0300

    aa

--- App/View/modules/Login/FormLogin.php
+++ App/View/modules/Login/FormLogin.php
@@ -1,7 +1,21 @@
-<form action="/login/auth" method="POST">
-    <label for="email">E-Mail:</label>
-    <input type="email" name="email" id="email" />
-    <label for="senha">senha:</label>
-    <input type="password" name="senha" id="email" />
-    <button type="submit">Entrar</button>
-</form>
+<div class="dropdown-menu">
+  <form class="px-4 py-3">
+    <div class="mb-3">
+      <label for="exampleDropdownFormEmail1" class="form-label">Email</label>
+      <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com">
+    </div>
+    <div class="mb-3">
+      <label for="exampleDropdownFormPassword1" class="form-label">Senha</label>
+      <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="123****">
+    </div>
+    <div class="mb-3">
+      <div class="form-check">
+        <input type="checkbox" class="form-check-input" id="dropdownCheck">
+        <label class="form-check-label" for="dropdownCheck">
+          Lembrar-me
+        </label>
+      </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Entrar</button>
+  </form>
+</div>
